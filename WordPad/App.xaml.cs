bde5c9b277f8ad5fda8eeee8c40ccdd3ee1a417d
Lixkote <95425619+Lixkote@users.noreply.github.com>
2023-02-10 17:06:31 +0100

    add sauce™
    Add WordPad11 Alpha 2 Souce Code

---
+++ WordPad/App.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace RectifyPad
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // CoreApplication.EnablePrelaunch was introduced in Windows 10 version 1607
+            bool canEnablePrelaunch = Windows.Foundation.Metadata.ApiInformation.IsMethodPresent("Windows.ApplicationModel.Core.CoreApplication", "EnablePrelaunch");
+
+            // NOTE: Only enable this code if you are targeting a version of Windows 10 prior to version 1607,
+            // and you want to opt out of prelaunch.
+            // In Windows 10 version 1511, all UWP apps were candidates for prelaunch.
+            // Starting in Windows 10 version 1607, the app must opt in to be prelaunched.
+            //if ( !canEnablePrelaunch && e.PrelaunchActivated == true)
+            //{
+            //    return;
+            //}
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                // On Windows 10 version 1607 or later, this code signals that this app wants to participate in prelaunch
+                if (canEnablePrelaunch)
+                {
+                    TryEnablePrelaunch();
+                }
+
+                // TODO: This is not a prelaunch activation. Perform operations which
+                // assume that the user explicitly launched the app such as updating
+                // the online presence of the user on a social network, updating a
+                // what's new feed, etc.
+
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+                ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
+                ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// This method should be called only when the caller
+        /// determines that we're running on a system that
+        /// supports CoreApplication.EnablePrelaunch.
+        /// </summary>
+        private void TryEnablePrelaunch()
+        {
+            Windows.ApplicationModel.Core.CoreApplication.EnablePrelaunch(true);
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ WordPad/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using WordPad.WordPadUI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RectifyPad
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public string FileString => "Document";
+        public string SeparatorType => " - ";
+        public string ApplicationName => "WordPad";
+        public string FullString => FileString + SeparatorType + ApplicationName;
+        public string ZoomString => ZoomSlider.Value.ToString() + "%";
+        public MainPage()
+        {
+            InitializeComponent();
+            Window.Current.SetTitleBar(AppTitleBar);
+        }
+        private async void Open_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker Picker = new FileOpenPicker();
+            Picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            Picker.FileTypeFilter.Add(".rtf");
+            Picker.FileTypeFilter.Add(".txt");
+
+            StorageFile file = await Picker.PickSingleFileAsync();
+
+
+
+            if (file != null)
+            {
+                tea.Text = file.DisplayName + " - WordPad";
+
+
+                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    IBuffer buffer = await FileIO.ReadBufferAsync(file);
+                    var reader = DataReader.FromBuffer(buffer);
+                    reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                    string text = reader.ReadString(buffer.Length);
+
+                    Editor.Document.LoadFromStream(TextSetOptions.FormatRtf, stream);
+
+                }
+
+                //Windows.Storage.AccessCache.StorageApplicationPermissions.MostRecentlyUsedList.Add(file);
+                //Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.AddOrReplace("CurrentlyOpenFile", file);
+            }
+        }
+
+        private void Undo_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Document.Undo();
+        }
+
+        private void Redo_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Document.Redo();
+        }
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Document.Selection.Cut();
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Document.Selection.Copy();
+        }
+
+        private void Paste_Click(Microsoft.UI.Xaml.Controls.SplitButton sender, Microsoft.UI.Xaml.Controls.SplitButtonClickEventArgs args)
+        {
+            Editor.Document.Selection.Paste(0);
+        }
+
+        private void ZoomSlider_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+
+        private void EditorContentHost_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            /*
+ 
+            The status bar is slightly tinted by the mica backdrop
+
+            Clipping the editor is needed, as the editor has a
+            shadow. Without the clip, the shadow would be visible
+            on the status bar
+
+            */
+
+            RectangleGeometry rectangle = new RectangleGeometry();
+            rectangle.Rect = new Rect(0, 0, EditorContentHost.ActualWidth, EditorContentHost.ActualHeight);
+            EditorContentHost.Clip = rectangle;
+        }
+
+        private async void Feedback_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(@"https://discord.com/invite/Btg3QTuS6c"));
+        }
+
+        private async void About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutDialog dialog = new AboutDialog();
+            await dialog.ShowAsync();
+
+        }
+    }
+}
