d4d8e5deb83c2711a1f48d0e9a9ecbc59c0af660
Lixkote <95425619+Lixkote@users.noreply.github.com>
2023-09-06 21:11:16 +0200

    some changes
    some changes

---
+++ WordPad/WordPadUI/TextRuler.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WordPad.WordPadUI
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class TextRuler : Page
+    {
+        public TextRuler()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
